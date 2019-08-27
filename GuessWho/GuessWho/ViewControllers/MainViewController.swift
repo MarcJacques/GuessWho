c8473fd9bc8244364d3a26fe9c6f0c494e9ee096
Luqmaan Khan <luq.j.khan@gmail.com>
2019-08-27 11:49:42 -0500

    Starter project

---
+++ GuessWho/GuessWho/ViewControllers/MainViewController.swift
@@ -0,0 +1,37 @@
+//
+//  MainViewController.swift
+//  GuessWho
+//
+//  Created by Luqmaan Khan on 8/26/19.
+//  Copyright © 2019 Luqmaan Khan. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupBackground()
+    }
+    
+    func setupBackground() {
+        let twitterBird = TwiterBirdView()
+        twitterBird.twitterImage = UIImage(named: "twitterBird")
+        twitterBird.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(twitterBird)
+        NSLayoutConstraint.activate([twitterBird.leadingAnchor.constraint(equalTo: view.leadingAnchor),twitterBird.trailingAnchor.constraint(equalTo: view.trailingAnchor),twitterBird.topAnchor.constraint(equalTo: view.topAnchor), twitterBird.bottomAnchor.constraint(equalTo: view.bottomAnchor)])
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
